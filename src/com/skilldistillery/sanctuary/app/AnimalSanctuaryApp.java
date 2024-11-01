9702de7a5c4eec5863226dfaf51f72bedd42aabf
Andrew Nelson <nels4766@gmail.com>
2024-11-01 17:40:24 -0600

    AS

--- src/com/skilldistillery/sanctuary/app/AnimalSanctuaryApp.java
+++ src/com/skilldistillery/sanctuary/app/AnimalSanctuaryApp.java
@@ -8,7 +8,16 @@ public class AnimalSanctuaryApp {
 		
 	Scanner kb = new Scanner(System.in);
 		
+	System.out.println("Choose your zoological options: ");
+		int choice = kb.nextInt();
 	
+	}
+	private static void printMenu() {
+		System.out.println("Choices");
+		System.out.println("1: Option");
+		System.out.println("2: Option");
+		System.out.println("3: Option");
+		System.out.println("4: Exit");
 	}
 
 }
---
+++ src/com/skilldistillery/sanctuary/entities/Sanctuary.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.sanctuary.entities;
+
+public class Sanctuary {
+	
+	private Animal [] sanctuaryAnimals = new Animal[5];
+	public void makeRounds() {
+		
+	}
+	
+
+}
